b1708b0999e502526363f26b30f72f64315b6ae3
Ofer Weiss <ofer.weiss@veeva.com>
2023-10-07 20:58:14 +0300

    Adding service and controller

---
+++ src/main/java/guru/springframework/spring5joksappv2/Spring5JoksAppV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5joksappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JoksAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JoksAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5joksappv2/Spring5JoksAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5joksappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JoksAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
